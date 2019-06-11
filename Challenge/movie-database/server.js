0bda4e42446be32dab14a2d9d1bbe0c336af28e5
nooraldin <nooraldin.alhamedi90@gmail.com>
2019-06-11 14:31:09 +0300

    step 3

--- Challenge/movie-database/server.js
+++ Challenge/movie-database/server.js
@@ -1,47 +1,37 @@
 
-/* const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
- */
-
-/*const port = 3500;
-app.listen(port, () => {  console.log('We are live on ' + port);});*/
-
-
+// Require express and create an instance of it
 var express = require('express');
 var app = express();
 
+// on the request to root (localhost:3000/)
 app.get('/', function (req, res) {
     res.send('OK');
 });
-
-// 1) Add a route that answers to all request types
-/* app.route('/article')
-.get(function(req, res) {
-    res.send('Get the article');
-})
-.post(function(req, res) {
-    res.send('Add an article');
-})
-.put(function(req, res) {
-    res.send('Update the article');
+/* req.params: {"status":"200", "message":"OK"}; */
+app.get('/test',function(req, res) {
+    res.send({status:200,message:"OK"});
 });
 
-// 2) Use a wildcard for a route
-// answers to : theANYman, thebatman, thesuperman
-app.get('/the*man', function(req, res) {
-    res.send('the*man');
+app.get('/time',function(req, res) {
+    var time=new Date();
+    res.send({status:200,message:time.getHours()+" :"
+    + time.getSeconds()+" "
+});
 });
 
-// 3) Use regular expressions in routes
-// responds to : batmobile, batwing, batcave, batarang
-app.get(/bat/, function(req, res) {
-    res.send('/bat/');
+
+/* 
+// On localhost:3000/welcome
+app.get('/welcome', function (req, res) {
+    res.send('<b>Hello</b> welcome to my http server made with express');
 });
 
+// Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-}); */
-
+});
+ */
+// start the server in the port 3000 !
 app.listen(3000, function () {
     console.log('Example app listening on port 3000.');
 });
